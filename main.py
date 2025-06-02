34e2b9ecff21e744b90e125ee87f4a63562f15a7
Aaron Lee <leeaaron326@gmail.com>
2025-06-01 22:01:03 -0400

    Added python-dotenv

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from sql-composer!")
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PROJECT_NAME = os.getenv("PROJECT_NAME")
+
+def main():
+    print(f"Hello from {PROJECT_NAME}!")
 
 if __name__ == "__main__":
     main()
