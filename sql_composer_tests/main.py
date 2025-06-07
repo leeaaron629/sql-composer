f2a838b61cba0a3228b36402e9665e2932d58297
Aaron Lee <leeaaron326@gmail.com>
2025-06-07 12:38:21 -0400

    Fixing formatting issues

--- sql_composer_tests/main.py
+++ sql_composer_tests/main.py
@@ -7,10 +7,12 @@ load_dotenv()
 
 PROJECT_NAME = os.getenv("PROJECT_NAME")
 
+
 def main():
     print(f"Hello from {PROJECT_NAME}!")
     table = Table(name="test", columns=[])
     print(table)
+
 
 if __name__ == "__main__":
     main()
